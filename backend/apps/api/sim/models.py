8160386108b3d736b79179ccc49b77459aebb05d
MrMoood <45488387+MrMoood@users.noreply.github.com>
2019-08-01 12:06:54 +0100

    Delete models.py

--- backend/apps/api/sim/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Pheno(models.Model):
-    name=models.CharField(max_length=200)
-class Disease(models.Model):
-    name=models.TextField()
